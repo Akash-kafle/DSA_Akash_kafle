253ba1455d457f234089722aa348f20936702da4
Akash kafle <aakashkafle47@gmail.com>
2024-04-10 05:42:33 +0100

    Few functions

--- Linked_list/src/function.cpp
+++ Linked_list/src/function.cpp
@@ -5,15 +5,48 @@ bool Linked_list::add(int data){
 }
 
 bool Linked_list::addToHead(int data){
-
+    Node * temp = new Node;
+    if(this->IsEmpty()){
+        temp->data = data;
+        temp->next = nullptr;
+        Head = temp;
+        Tail = temp;
+        std::cout<<"Sucess"<<std::endl;
+        return true;
+    }
+    else{
+        temp->data = data;
+        temp->next = Head->next;
+        Head = temp;
+        std::cout<<"Sucess"<<std::endl;
+        return true;
+    }
+    return false;
 }
 
 bool Linked_list::addToTail(int data){
-
+    Node * temp = new Node;
+    if(this->IsEmpty()){
+        temp->data = data;
+        temp->next = nullptr;
+        Head = temp;
+        Tail = temp;
+        std::cout<<"Sucess"<<std::endl;
+        return true;
+    }
+    else{
+        temp->data = data;
+        temp->next = nullptr;
+        Tail->next = temp ;
+        Tail = temp;
+        std::cout<<"Success"<<std::endl;
+        return true;
+    }
+    return false;
 }
 
 bool Linked_list::remove(int data){
-
+    
 }
 
 bool Linked_list::removeFromHead(int &data){
