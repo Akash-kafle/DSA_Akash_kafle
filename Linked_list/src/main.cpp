fb335c85df85d88fdaf57300f8d3626f911aadaf
Akash kafle <aakashkafle47@gmail.com>
2024-05-04 20:27:59 +0545

    final structuring of the code

--- Linked_list/src/main.cpp
+++ Linked_list/src/main.cpp
@@ -22,16 +22,24 @@ int main()
         std::cin.clear();
         std::cin.ignore(1000, '\n');
     } while (true);
+    std::cout << "\nBefore data adding" << std::endl;
+    List.print();
+
     for (int i = 1; i <= num; i++)
     {
         List.addToTail(i);
     }
+    List.addToHead(0);
+    List.add(num, 1);
+    std::cout << "\nAdded data " << std::endl;
     List.print();
 
     List.removeFromHead(data);
-    std::cout << "data : " << data << std::endl;
+    std::cout << "\nRemoved data : " << data << std::endl;
+    std::cout << "\nAfter removing from head:" << std::endl;
     List.print();
     List.~Linked_list();
+    std::cout << "\nAfter deleting the list:" << std::endl;
     List.print();
     return 0;
 }
